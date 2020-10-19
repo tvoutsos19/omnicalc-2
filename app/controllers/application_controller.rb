04cbc826ec92a9695aba640e3ed4ef02c2346891
tvoutsos <tvoutsos@chicagobooth.edu>
2020-10-19 21:52:45 +0000

    First commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,42 @@
 class ApplicationController < ActionController::Base
+  
+  def add
+    render ({ :template => "operations/addition.html.erb" })   
+  end
+
+  def add_results
+    # Parameters: {"first_num"=>"3", "second_num"=>"4"}
+    @first = params.fetch("first_num")
+    @second = params.fetch("second_num")
+    @results = (@first.to_f+@second.to_f).to_f
+    render ({ :template => "operations/add_results.html.erb" })   
+  end
+
+  def subtract
+    render ({ :template => "operations/subtraction.html.erb" })
+  end
+
+  def subtract_results
+    @first = params.fetch("first_num")
+    @second = params.fetch("second_num")
+    @results = (@first.to_f - @second.to_f).to_f
+    render ({ :template => "operations/subtract_results.html.erb" })
+  end
+
+  def multiply
+    render ({ :template => "operations/multiplication.html.erb" })
+  end
+
+  def mutliply_results
+    @first = params.fetch("first_num")
+    @second = params.fetch("second_num")
+    @results = (@first.to_f * @second.to_f).to_f
+    render ({ :template => "operations/multiply_results.html.erb"})
+
+  end
+
+  def divide
+    render ({ :template => "operations/division.html.erb" })
+  end 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
 
+  get("/add", { :controller => "application", :action => "add"})
+
+  get("/wizard_add", { :controller => "application", :action => "add_results"})
+
+  get("/subtract", { :controller => "application", :action => "subtract"})
+
+  get("/wizard_subtract", { :controller=> "application", :action => "subtract_results"})
+
+  get("/multiply", { :controller => "application", :action => "multiply"})
+
+  get("/wizard_multiply", { :controller => "application", :action => "multiply_results"})
+
+  get("/divide", { :controller => "application", :action => "divide"})
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
